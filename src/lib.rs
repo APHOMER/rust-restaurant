5d02a3143c15c166ce1a85bd08171fff9771784e
Aphomer <aaphomer@gmail.com>
2025-01-18 07:44:03 +0100

    worked on use..

--- src/lib.rs
+++ src/lib.rs
@@ -15,30 +15,115 @@
 
 
 
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+// mod front_of_house {
+//     mod hosting {
+//         fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
-    }
+//         fn seat_at_table() {}
+//     }
 
-    mod serving {
-        fn take_order() {}
+//     mod serving {
+//         fn take_order() {}
 
-        fn serve_order() {}
+//         fn serve_order() {}
 
-        fn take_payment() {}
-    }
+//         fn take_payment() {}
+//     }
+// }
+
+
+
+// mod front_of_house {
+//     mod hosting {
+//         fn add_to_waitlist() {}
+//     }
+// }
+
+// pub fn eat_at_restaurant() {
+//     // Absolute path
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+//     // Relative path
+//     front_of_house::hosting::add_to_waitlist();
+// }
+
+
+
+// mod back_of_house {
+//     pub struct Breakfast {
+//         pub toast: String,
+//         seasonal_fruit: String,
+//     }
+
+//     impl Breakfast {
+//         pub fn summer(toast: &str) -> Breakfast {
+//             Breakfast {
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("peaches"),
+//             }
+//         }
+//     }
+// }
+
+
+// // Making Structs and Enums Public
+// // Listing 7-10: Designating an enum as public makes all its variants public
+// pub fn eat_at_restaurant() {
+//     // Order a breakfast in the summer with Rye toast
+//     let mut meal = back_of_house::Breakfast::summer("Rye");
+//     // Change our mind about what bread we'd like
+//     meal.toast = String::from("Wheat");
+//     println!("I'd like {} toast please", meal.toast);
+
+//     // The next line won't compile if we uncomment it; we're not allowed
+//     // to see or modify the seasonal fruit that comes with the meal
+//     // meal.seasonal_fruit = String::from("blueberries");
+// }
+
+
+
+
+// Bringing Paths into Scope with the use Keyword
+
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// use crate::front_of_house::hosting;
+
+// pub fn eat_at_restaurant() {
+//     hosting::add_to_waitlist();
+// }
+
+// ///------------------------------------>>>>>
+
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//     }
+// }
+
+// use crate::front_of_house::hosting::add_to_waitlist;
+
+// pub fn eat_at_restaurant() {
+//     add_to_waitlist();
+// }
+
+
+
+
+// MAP
+
+use std::collections::HashMap;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, 2);
+
+    // println!("The map elements are {}", map);
 }
-
-
-
-
-
-
-
-
-
 
 
 
